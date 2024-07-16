c2f79de17db81759f6eebd12d20986022e326f49
DanielBretts <daniel.1481997@gmail.com>
2024-07-16 15:48:08 +0300

    fixed the welcome view

--- iosCardGame/WelcomeViewController.swift
+++ iosCardGame/WelcomeViewController.swift
@@ -11,35 +11,41 @@ import UIKit
 class WelcomeViewController: UIViewController,UITextFieldDelegate{
     
     @IBOutlet weak var LBL_welcome: UILabel!
-    @IBOutlet var BTN_submitName: UIView!
     @IBOutlet weak var TF_insertName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        TF_insertName.delegate = self
         TF_insertName.isUserInteractionEnabled = true
         checkNameExist();
     }
     
-    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder() // Dismiss the keyboard
+            
+        if let text = textField.text {
+            setName(text:text)
+        }
+            
+        return true
+    }
     
     func checkNameExist(){
         if let value = UserDefaults.standard.string(forKey: "username"){
             LBL_welcome.isHidden = false
+            TF_insertName.isHidden = true
             LBL_welcome.text = "Hi \(value)"
         }else{
             LBL_welcome.isHidden = true
             TF_insertName.isHidden = false
-            BTN_submitName.isHidden = false
         }
     }
     
-    @IBAction func setName(_ sender: Any) {
-        if let username : String = TF_insertName.text {
-            UserDefaults.standard.set(username, forKey: "username")
-            LBL_welcome.text = "Hi \(username)"
-            LBL_welcome.isHidden = false
-//            BTN_submitName.isHidden = true
-        }
-        
+    func setName(text : String) {
+        UserDefaults.standard.set(text, forKey: "username")
+        TF_insertName.isHidden = true
+        LBL_welcome.text = "Hi \(text)"
+        LBL_welcome.isHidden = false
     }
     
     @IBAction func startGame(_ sender: Any) {
